4491eca052c3fdd977614531ed05a693c30472a1
yassin samir <yassinsam3@gmail.com>
2024-08-03 19:12:10 +0300

    improving the ui of the notfound page by taking 100% of the viewport height to be centered in the page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,18 +3,20 @@ import React from "react";
 
 function notFound() {
   return (
-    <div className=" flex flex-col items-center justify-center    h-[59vh] ">
+    <div className="flex flex-col items-center justify-center text-center h-[100vh] ">
       <h1 className="text-6xl md:text-[200px] font-extrabold text-white">
         404
       </h1>
-      <p className="text-3xl md:text-5xl font-extrabold text-white">Page not found</p>
-      <p className=" text-sm text-[#7b7a7a]  mt-5 uppercase">
+      <p className="text-3xl md:text-5xl font-extrabold text-white">
+        Page not found
+      </p>
+      <p className=" text-sm text-greyShade mt-3 uppercase">
         The page you are looking for does not exist
       </p>
       <Link
         href={"/"}
         className="
-          text-white uppercase mt-3 
+        text-white uppercase mt-3 
           transition ease-linear duration-200 
           text-xs
          hover:text-main"
