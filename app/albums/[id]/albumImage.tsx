575d119fe15ab69238654cf846b61f921ddcc85d
yassin samir <yassinsam3@gmail.com>
2024-08-14 03:52:38 +0300

    adding priority property to next/image component

--- app/albums/[id]/albumImage.tsx
+++ app/albums/[id]/albumImage.tsx
@@ -25,6 +25,7 @@ function AlbumImage({
         alt={title}
         width={100}
         height={100}
+        priority
         className={`w-full h-full md:max-w-[400px] md:max-h-[400px] 
         object-contain transition-opacity select-none  ${
           !Loaded ? "absolute z-[-1] opacity-0 inset-0" : ""
